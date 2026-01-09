64879c1f837e0691a6a586bd6c7eceac2d9211ae
trkbt10 <triplequartet@gmail.com>
2026-01-09 22:13:49 +0900

    Remove demo application files and configurations, including styles, server, runner, types, TypeScript configuration, and Vite setup.

--- demo/api/routes/files.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-import { findPptxFiles } from "../services/pptx-loader";
-
-export const filesRouter = new Hono();
-
-filesRouter.get("/", (c) => {
-  const files = findPptxFiles();
-  return c.json(files);
-});
--- demo/app/renderers/index.ts
+++
@@ -1,4 +0,0 @@
-export type { RenderMode, SlideRenderer, AnimatedSlideRenderer, RenderResult, RendererOptions, TimingData, SlideSize } from "./types";
-export { svgRenderer } from "./svg-renderer";
-export { htmlRenderer } from "./html-renderer";
-export { createAnimRenderer } from "./anim-renderer";
